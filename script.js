0f0a1509dfc3c528e4bd656ae952182a6bd990e6
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2024-01-24 16:38:47 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,11 @@
  if (t >= 7 && t <= 17){
 
    document.getElementById('day').style.display = "block";
+   document.getElementById('par').style.display = "block";
  }
 else {
 
    document.getElementById('night').style.display = "block";
+   document.getElementById('pa').style.display = "block";
+
 }
